1221e7ca427eff7e2c540b583f13d0a252d2c384
alex3190 <alexandra.bulearca@gmail.com>
2017-09-15 13:48:19 +0000

    fixed homepage button

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -1,7 +1,9 @@
 @extends('navigation')
 @section('content')
 {{--main section--}}
-        <a href={{url('/events/create')}}} type="button" class="btn btn-success">Start your quest!</a>
+
+        <a href="{{url('/events/create')}}" class="btn btn-success"> Start your quest!</a>
+
 {{--insert news here--}}
     
 @endsection
