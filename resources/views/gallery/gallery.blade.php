de9e569802e3b4899cd89e326b248753a8918e06
semeth <thesemeth@gmail.com>
2017-10-04 22:01:57 +0300

    gallery

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -1,4 +1,18 @@
 @extends('navigation')
 @section('content')
-    {{--code html here--}}
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                Testing
+                </div>
+                <div class="col-md-6">
+                
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+        
+        </div>
+    </div>
 @endsection
