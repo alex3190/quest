e957edea83ae9014ea8936fce973b8258a8b1f22
alexandra.bulearca <alexandra.bulearca@gfm-research.com>
2018-05-10 14:50:14 +0300

    fixed all footer issues

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -26,83 +26,82 @@
         });
     </script>
 </head>
-<div class="wrapper">
-<body class="body">
-
-<nav class="navbar navbar-default navbar-doublerow navbar-trans navbar-fixed-top">
-    <!-- top nav -->
-    <nav class="navbar navbar-top hidden-xs">
-        <div class="container">
-            <!-- left nav top -->
-            <ul class="nav navbar-nav pull-left">
-                <li><a href="#"><span class="text-white">Welcome Home, traveler! </span></a></li>
-                <li><a href="{{url('/adventures')}}"><span class="text-white"><strong>Start a new adventure?</strong></span></a></li>
-                @if($isLoggedIn)
-                <li><a href="{{url('/user/' . $user->id . '/adventures')}}"><span class="text-white">Or revisit one you're already in?</span></a></li>
-                @endif
-            </ul>
-            <!-- right nav top -->
-            <ul class="nav navbar-nav pull-right">
-                <li><a href="{{url('/about')}}">About Us</a></li>
-                <li><a href="{{url('/about')}}">Contact Us</a></li>
-            </ul>
-        </div>
-        <div class="dividline light-grey"></div>
-    </nav>
-    <!-- down nav -->
-    <nav class="navbar navbar-down">
-        <div class="container">
-            <div class="flex-container">
-                <div class="navbar-header flex-item">
-                    <a class="navbar-brand" href="{{url('/')}}"><strong>A quest to remember</strong></a>
-                </div>
-                <ul class="nav navbar-nav flex-item hidden-xs">
-                    <li class="dropdown"><a href="{{url('/gallery')}}">Gallery</a></li>
-                    <li class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle dropdown-menu-bg" href="#">Adventures <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{url('/adventures')}}">Join an existing party</a></li>
-                            <li><a href="{{url('/adventures/create')}}">Create a new party</a></li>
-                        </ul>
-                    </li>
+<div id="holder">
+    <body class="body">
+    <nav class="navbar navbar-default navbar-doublerow navbar-trans navbar-fixed-top">
+        <!-- top nav -->
+        <nav class="navbar navbar-top hidden-xs">
+            <div class="container">
+                <!-- left nav top -->
+                <ul class="nav navbar-nav pull-left">
+                    <li><a href="#"><span class="text-white">Welcome Home, traveler! </span></a></li>
+                    <li><a href="{{url('/adventures')}}"><span class="text-white"><strong>Start a new adventure?</strong></span></a></li>
                     @if($isLoggedIn)
-
-                        <li class="dropdown">
-                            <a data-toggle="dropdown" class="dropdown-toggle dropdown-menu-bg" href="#"> My profile <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="{{url('/user/' . $user->id . '/adventures')}}">My adventures</a></li>
-                            </ul>
-                        </li>
+                        <li><a href="{{url('/user/' . $user->id . '/adventures')}}"><span class="text-white">Or revisit one you're already in?</span></a></li>
                     @endif
                 </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    @if($isLoggedIn)
-                        <li><a href="{{ route('logout') }}">Logout</a></li>
-                    @else
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                    @endif
+                <!-- right nav top -->
+                <ul class="nav navbar-nav pull-right">
+                    <li><a href="{{url('/about')}}">About Us</a></li>
+                    <li><a href="{{url('/about')}}">Contact Us</a></li>
                 </ul>
             </div>
-        </div>
+            <div class="dividline light-grey"></div>
+        </nav>
+        <!-- down nav -->
+        <nav class="navbar navbar-down">
+            <div class="container">
+                <div class="flex-container">
+                    <div class="navbar-header flex-item">
+                        <a class="navbar-brand" href="{{url('/')}}"><strong>A quest to remember</strong></a>
+                    </div>
+                    <ul class="nav navbar-nav flex-item hidden-xs">
+                        <li class="dropdown"><a href="{{url('/gallery')}}">Gallery</a></li>
+                        <li class="dropdown">
+                            <a data-toggle="dropdown" class="dropdown-toggle dropdown-menu-bg" href="#">Adventures <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{url('/adventures')}}">Join an existing party</a></li>
+                                <li><a href="{{url('/adventures/create')}}">Create a new party</a></li>
+                            </ul>
+                        </li>
+                        @if($isLoggedIn)
+
+                            <li class="dropdown">
+                                <a data-toggle="dropdown" class="dropdown-toggle dropdown-menu-bg" href="#"> My profile <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="{{url('/user/' . $user->id . '/adventures')}}">My adventures</a></li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        @if($isLoggedIn)
+                            <li><a href="{{ route('logout') }}">Logout</a></li>
+                        @else
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </nav>
-</nav>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
-@include('flash::message')
-@yield('content')
+    @include('flash::message')
+    @yield('content')
 
-</body>
+    </body>
 </div>
-<br>
+
 <div class="footer-container">
-    <footer class="footer-distributed custom navbar-fixed-bottom">
+    <footer class="footer-distributed custom">
         <script src="{{ asset('js/shuffle.min.js') }}"></script>
         <script src="{{ asset('js/shuffle_additional.js') }}"></script>
 
@@ -146,10 +145,10 @@
                 You are the storyteller and story, the ink and the writer, the play and the audience, the master and the slayer of dragons.
             </p>
             {{--<div class="footer-icons">--}}
-                {{--<a href="#"><i class="fa fa-facebook"></i></a>--}}
-                {{--<a href="#"><i class="fa fa-twitter"></i></a>--}}
-                {{--<a href="#"><i class="fa fa-linkedin"></i></a>--}}
-                {{--<a href="#"><i class="fa fa-github"></i></a>--}}
+            {{--<a href="#"><i class="fa fa-facebook"></i></a>--}}
+            {{--<a href="#"><i class="fa fa-twitter"></i></a>--}}
+            {{--<a href="#"><i class="fa fa-linkedin"></i></a>--}}
+            {{--<a href="#"><i class="fa fa-github"></i></a>--}}
 
             {{--</div>--}}
 
